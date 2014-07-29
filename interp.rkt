6e7f094cb4d0f30d3859ddcc61d71eea43e248d5
Zhang Shuai <satan.student@gmail.com>
2014-07-29 16:16:42 +0800

    Finish parser.

--- interp.rkt
+++ interp.rkt
@@ -25,15 +25,31 @@
    [(:or "if" "then" "else" "endif") (string->symbol (string-upcase lexeme))]
    [(:+ numeric) (token-INT (string->number lexeme))]
    [(:+ alphabetic) (token-ID lexeme)]
+   [#\" (token-STRING (list->string (string-lexer input-port)))]
+   [(:: #\/ #\/ (:* (:- any-char #\newline)) #\newline) (return-without-pos (snail-lexer input-port))]
    [whitespace (return-without-pos (snail-lexer input-port))]
    [(eof) (token-EOF)]))
+
+(define string-lexer
+  (lexer
+   [(:~ #\" #\\) (cons (car (string->list lexeme))
+                       (string-lexer input-port))]
+   [(:: #\\ #\\) (cons #\\ (string-lexer input-port))]
+   [(:: #\\ #\") (cons #\" (string-lexer input-port))]
+   [#\" null]))
 
 (define snail-parser
   (parser
    [src-pos]
    [start prog]
    [end EOF]
-   [error (lambda (ok name val start end) (display name))]
+   [error (lambda (ok name val start end)
+            (printf
+             "parsing ~a with value ~a error. POS: [~a, ~a]\n"
+             name
+             val
+             start
+             end))]
    [tokens primitives operators delimiters keywords]
    [precs (right =)
           (left < > <= >= == !=)
@@ -51,7 +67,8 @@
                   [(print_stmt) $1])
     (assign_stmt  [(ID = expr) `(ASSIGN ,$1 ,$3)])
     (print_stmt   [(PRINT expr) `(PRINT ,$2)]
-                  [(PRINT NEWLINE) '(PRINT-NEWLINE)])
+                  [(PRINT NEWLINE) '(PRINT-NEWLINE)]
+                  [(PRINT STRING) `(PRINT ,$2)])
     (compond_stmt [(IF expr THEN stmt_list ENDIF)
                    `(IF ,$2 ,(reverse $4))]
                   [(IF expr THEN stmt_list ELSE stmt_list ENDIF)
@@ -70,7 +87,7 @@
                   [(LPAR expr RPAR)    $2]
                   [(INT)               $1]
                   [(ID)                $1])]))
-   
+
 (define (run-lexer lexer port)
   (let* ([t (position-token-token (lexer port))]
          [tn (token-name t)])
@@ -79,19 +96,33 @@
       [else
        (begin
          (display t)
-         (display #\newline)
+         (newline)
          (run-lexer lexer port))])))
+
+(define (lex-this lexer input) (lambda () (lexer input)))
+
+(let ([input (open-input-string "print \"hello\";")])
+  (display (snail-parser (lex-this snail-lexer input))))
+(newline)
 
 (run-lexer snail-lexer (open-input-string "x = 2; print x;"))
 (run-lexer snail-lexer (open-input-string "id = 123;"))
 (run-lexer snail-lexer (open-input-string "if a < 3 then 0 else 7 endif print newline;"))
+(run-lexer snail-lexer (open-input-string "a = \"abc\";"))
+(run-lexer snail-lexer (open-input-string "a = \"abc\"; b = \"bcd\"; print b;"))
+(run-lexer snail-lexer (open-input-string "y = 20; print \"the value of x is \"; print x; print newline;"))
 
-(define (lex-this lexer input) (lambda () (lexer input)))
 (let ([input (open-input-string "a = (1 + b) * - 3 < 0; print a;")])
   (display (snail-parser (lex-this snail-lexer input))))
-(display #\newline)
+(newline)
 (let ([input (open-input-string "a = 1 + 2 * b; if a > 0 then print a; endif")])
   (display (snail-parser (lex-this snail-lexer input))))
-(display #\newline)
+(newline)
 (let ([input (open-input-string "a = 1 + 2 * b; if a > 0 then print a; else a = 0; print b; endif")])
   (display (snail-parser (lex-this snail-lexer input))))
+(newline)
+(let ([input (open-input-file "sample-code.txt" #:mode 'text)])
+  (begin
+    (display (snail-parser (lex-this snail-lexer input)))
+    (close-input-port input)))
+(newline)
